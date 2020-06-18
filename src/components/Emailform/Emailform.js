d97789c5cf928f919a0bc31864b0312385c6de1f
Daniel Munk Torres <5866081@RTS-365.dk>
2020-06-18 14:02:23 +0200

    add form

--- src/components/Emailform/Emailform.js
+++ src/components/Emailform/Emailform.js
@@ -1,8 +1,9 @@
 import React from "react"
+import styled from "styled-components"
 
 const Emailform = () => {
   return (
-    <div>
+    <StyledEmailform>
       <form
         name="contact"
         method="post"
@@ -10,12 +11,45 @@ const Emailform = () => {
         data-netlify-honeypot="bot-field"
       >
         <input name="form-name" type="hidden" value="contact" />
+        <label htmlFor="name">Full Name</label>
         <input type="text" name="name" />
+        <label htmlFor="email">Enter Your Email Here..</label>
         <input type="text" name="email" />
-        <button>Send</button>
+        <button>Send Email</button>
       </form>
-    </div>
+    </StyledEmailform>
   )
 }
 
 export default Emailform
+
+const StyledEmailform = styled.section`
+  width: 350px;
+  height: 350px;
+  border-radius: 20px;
+  background-color: coral;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  input {
+    border-radius: 20px;
+    border: none;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+
+  button {
+    display: flex;
+    flex-direction: column;
+    border-radius: 20px;
+    border: none;
+    width: 100px;
+    background-color: greenyellow;
+    color: black;
+  }
+`
