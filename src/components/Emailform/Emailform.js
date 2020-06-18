bd44f7c0b056449f77673f9d1ee529ae97b8c6d5
Daniel Munk Torres <5866081@RTS-365.dk>
2020-06-18 10:56:21 +0200

    add

--- src/components/Emailform/Emailform.js
+++ src/components/Emailform/Emailform.js
@@ -9,7 +9,7 @@ const Emailform = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
-        <input name="contact" type="hidden" value="contact" />
+        <input name="form-name" type="hidden" value="contact" />
         <input type="text" name="name" />
         <button>Send</button>
       </form>
